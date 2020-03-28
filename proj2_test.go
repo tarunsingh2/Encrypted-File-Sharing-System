5ad7ba924c768d24306bf86b95518b0ce3713be5
Chris Park <45411746+junkin7@users.noreply.github.com>
2020-03-27 21:24:33 -0700

    user tests

--- proj2_test.go
+++ proj2_test.go
@@ -21,13 +21,16 @@ func clear() {
 	userlib.KeystoreClear()
 }
 
-func TestInit(t *testing.T) {
+// [USER TESTS] Testing the instantiation and get of users
+// Simple initialization test
+func TestUsers1(t *testing.T) {
 	clear()
-	t.Log("Initialization test")
-
 	// You can set this to false!
 	userlib.SetDebugStatus(true)
 
+	t.Log("Simple Init and Get Test")
+
+	// Creating user alice
 	u, err := InitUser("alice", "fubar")
 	if err != nil {
 		// t.Error says the test fails
@@ -35,11 +38,109 @@ func TestInit(t *testing.T) {
 		return
 	}
 	// t.Log() only produces output if you run with "go test -v"
-	t.Log("Got user", u)
+	t.Log("Instantiated user ->", u.Username)
 	// If you want to comment the line above,
 	// write _ = u here to make the compiler happy
 	// You probably want many more tests here.
+
+
+	// Should return alice correctly
+	get_u, err := GetUser("alice", "fubar")
+	if err != nil {
+		// t.Error says the test fails
+		t.Error("Failed to get user", err)
+		return
+	}
+	t.Log("Got user -> ", get_u.Username)
+
+
+	// Wrong password should not return user
+	_, wrongpass_err := GetUser("alice", "wrongpass")
+	if wrongpass_err == nil {
+		t.Error("Wrong password should not allow access to user")
+		return
+	}
+	t.Log("Wrong password did not return user info ->", wrongpass_err)
+
+
+	t.Log("No Username Test")
+	// We can assume that username isn't empty but what about the password?
+	_, user_err := InitUser("", "x")
+	if user_err == nil {
+		// t.Error says the test fails
+		t.Error("Should not be able to create accounts with no username -> ", user_err)
+		return
+	}
+
+
+	t.Log("No Password Test")
+	// We can assume that username isn't empty but what about the password?
+	_, pass_err := InitUser("Bob", "")
+	if pass_err == nil {
+		// t.Error says the test fails
+		t.Error("Should not be able to create accounts with no password -> ", pass_err)
+		return
+	}
 }
+
+// Username duplication test. Should return an error on the second initialization
+func TestUsers2(t *testing.T) {
+	clear()
+	t.Log("Users Test 2 - Duplication Initialization")
+
+	// You can set this to false!
+	userlib.SetDebugStatus(true)
+
+	_, err := InitUser("alice", "fubar")
+	if err != nil {
+		// t.Error says the test fails
+		t.Error("Failed to initialize user", err)
+		return
+	}
+
+	u, err := InitUser("alice", "fubar")
+	// Should error as the username alice already exists
+	if err != nil {
+		t.Log("Should return an error when trying to create an account with an existing username-> ", err)
+	} else {
+		t.Error("Created a new user when it wasn't supposed to", u)
+	}
+}
+
+// Trying to create an account with a very long username
+func TestUsers3(t *testing.T) {
+	clear()
+	t.Log("Users Test 3 - Long Username Initalization")
+
+	// You can set this to false!
+	userlib.SetDebugStatus(true)
+
+	u, err := InitUser("alice", "fubar")
+	// Should error as the username alice already exists
+	if err != nil {
+		// t.Error says the test fails
+		t.Error("Failed to initialize user", err)
+		return
+	}
+
+	username := "Barry"
+	for i := 0; i < 10000; i++ {
+		username = username + "barry"
+	}
+	_, long_err := InitUser(username, "fubar")
+	if err != nil {
+		t.Error("Failed to instantiate user with long username -> ", long_err)
+	}
+
+	// Alice's user struct should be intact
+	same_u, err := GetUser("alice", "fubar")
+	if err != nil {
+		t.Error("Failed to get user -> ", err)
+	} else if u.Username != same_u.Username {
+		t.Error("Alice's user data should be untouched")
+	}
+}
+
 
 func TestStorage(t *testing.T) {
 	clear()
@@ -94,7 +195,7 @@ func TestShare(t *testing.T) {
 
 	v := []byte("This is a test")
 	u.StoreFile("file1", v)
-	
+
 	var v2 []byte
 	var magic_string string
 
